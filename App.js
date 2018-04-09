b58c7f8df103cbade22523b3f33cf96be5a66fbb
Anusha Kokkula <anusha.kokkula@ge.com>
2018-04-09 17:04:11 -0500

    Adding image

--- App.js
+++ App.js
@@ -1,14 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <MyImage imgName='./images/Cartoon.png' />
+    );
+  }
+}
+
+class MyImage extends Component {
+  render() {
+    let pic = {
+      uri: this.props.imgName
+    };
+    return (
+      <Image source={require(pic)} style={{width: 220, height: 150,}} />
     );
   }
 }
@@ -20,4 +27,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  image: {
+    flex: 1,
+  }
 });
